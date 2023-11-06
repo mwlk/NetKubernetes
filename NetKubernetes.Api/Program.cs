f62e7e1b411be273fe00b98cad9982459f2f3a2a
Mirko Wlk <mirkoivowlk@gmail.com>
2023-11-06 03:58:30 -0300

    update program class

--- NetKubernetes.Api/Program.cs
+++ NetKubernetes.Api/Program.cs
@@ -1,13 +1,93 @@
+using System.Text;
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Netkubernetes.Repository.Interfaces;
 using NetKubernetes.Middleware;
+using NetKubernetes.Models;
+using NetKubernetes.Persistence;
+using NetKubernetes.Profiles;
+using NetKubernetes.Repository.Classes;
+using NetKubernetes.Repository.Interfaces;
+using NetKubernetes.Token.Classes;
+using NetKubernetes.Token.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt
+        .LogTo(Console.WriteLine,
+            new[] { DbLoggerCategory.Database.Command.Name },
+            LogLevel.Information)
+        .EnableSensitiveDataLogging();
+    opt
+        .UseSqlServer(builder.Configuration
+        .GetConnectionString("SqlServerConnection")!);
+});
+
+builder.Services.AddScoped<IPropertiesRepository, PropertiesRepository>();
+//! revisar si no va despues
+builder.Services.AddScoped<IUsersRepository, UsersRepository>();
+
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt =>
+{
+    var policy = new AuthorizationPolicyBuilder()
+    .RequireAuthenticatedUser()
+    .Build();
+
+    opt.Filters.Add(new AuthorizeFilter(policy));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var mapperConfig = new MapperConfiguration(opt =>
+{
+    opt.AddProfile(new PropertyProfile());
+});
+
+IMapper mapper = mapperConfig.CreateMapper();
+builder.Services.AddSingleton(mapper);
+
+var builderSecurity = builder.Services.AddIdentityCore<User>();
+var identityBuilder = new IdentityBuilder(builderSecurity.UserType, builder.Services);
+identityBuilder.AddEntityFrameworkStores<AppDbContext>();
+identityBuilder.AddSignInManager<SignInManager<User>>();
+
+builder.Services.AddSingleton<ISystemClock, SystemClock>();
+
+builder.Services.AddScoped<IJwtGenerator, JwtGenerator>();
+
+builder.Services.AddScoped<IUserSession, UserSession>();
+
+var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("My_Secret"));
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(opt =>
+    {
+        opt.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = key,
+            ValidateAudience = false,
+            ValidateIssuer = false
+        };
+    });
+
+builder.Services.AddCors(opt => opt.AddPolicy("appcors", builder =>
+{
+    builder.WithOrigins("*")
+    .AllowAnyMethod()
+    .AllowAnyHeader();
+}));
 
 var app = builder.Build();
 
